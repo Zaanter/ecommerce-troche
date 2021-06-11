9086ef36a0fe6407efd976bd6330205f0e8a6744
Agustin Troche <agustin.troche@everis.nttdata.com>
2021-06-11 00:07:08 -0300

    Se mejoro context

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,15 @@
+import { Button, Container } from 'react-bootstrap';
 import React, {useContext} from 'react'
 
 import { CartContext } from '../context/CartProvider';
-import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
     const cart = useContext(CartContext)
-
     return ( 
         <Container>
-            {cart && <h3>Se han registrado items en el carrito.</h3>}
+            {cart && <h3>Se han registrado {cart.cartItems.length} items en el carrito.</h3>}
+            <Button><Link to='/' style={{color:'white', textDecoration:'none'}}>Regresar al home</Link></Button>
         </Container>
      );
 }
